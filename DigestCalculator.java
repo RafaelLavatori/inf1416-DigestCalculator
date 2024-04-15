2bb01337cae8a5875bd7ff14db0b1e0a62e9a92d
João Couto <joaoqcouto@gmail.com>
2024-04-14 21:22:53 -0300

    Implement XmlHandler class

--- DigestCalculator.java
+++ DigestCalculator.java
@@ -1,110 +1,16 @@
 import java.security.*;
-import javax.crypto.*;
 import java.io.*;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 
 // Generate a DigestCalculator
 public class DigestCalculator {
-    
-    //Cria arquivo xml 
-    private static void CreateXMLFileInJava () {
-        
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder dBuilder;
-        
-        try {
-            dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.newDocument();
-            
-            // add elements to Document
-            Element rootElement = doc.createElement("CATALOG");
-            
-            // append root element to document
-            doc.appendChild(rootElement);
-
-            // append first child element to root element
-            rootElement.appendChild(createFile_EntryElement(doc, "name1", "MD5", "ffffeeee"));
-
-            // append second child
-            rootElement.appendChild(createFile_EntryElement(doc, "name2", "SHA256", "eeeeffff"));
-
-            // for output to file, console
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-            
-            // for pretty print
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            DOMSource source = new DOMSource(doc);
-
-            // write to console or file
-            StreamResult console = new StreamResult(System.out);
-            StreamResult file = new StreamResult(new File("list_digest.xml"));
-
-            // write data
-            transformer.transform(source, console);
-            transformer.transform(source, file);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    
-    //Cria Uma FileEnrty
-    private static Node createFile_EntryElement(Document doc, String file_name, String tipo_digest, String digest_hex) {
-            
-        Element file_entry = doc.createElement("FILE_ENTRY");
-
-        // Cria file_name element
-        file_entry.appendChild(createFinalElements(doc, "FILE_NAME", file_name));
-
-        // Cria digest_entry element
-        file_entry.appendChild(createDigest_EntryElement(doc, tipo_digest, digest_hex));
-
-
-        return file_entry;
-    }
-    
-    //Cria Uma Digest Entry
-    private static Node createDigest_EntryElement(Document doc, String tipo_digest, String digest_hex) {
-            
-        Element digest_entry = doc.createElement("DIGEST_ENTRY");
-
-        // Cria tipo_digest element
-        digest_entry.appendChild(createFinalElements(doc, "DIGEST_TYPE", tipo_digest));
-
-        // Cria digest_hex element
-        digest_entry.appendChild(createFinalElements(doc, "DIGEST_HEX", digest_hex));
-
-
-        return digest_entry;
-    }
-
-    // Cria nó da arvore
-    private static Node createFinalElements(Document doc, String name, String value) {
-        Element node = doc.createElement(name);
-        node.appendChild(doc.createTextNode(value));
-        return node;
-    }
-    
-    private static void file_digest(String inputFile, String tipo_digest) throws Exception {
+    private static String file_digest(String inputFile, String tipo_digest) throws Exception {
         try (
             InputStream inputStream = new FileInputStream(inputFile);
         ) {
             long fileSize = new File(inputFile).length();
             byte[] allBytes = new byte[(int) fileSize];
          
-            int bytesRead = inputStream.read(allBytes);
-            
+            // int bytesRead = inputStream.read(allBytes);
             //imprime os bytes lidos do arquivo        
             //for(int i =0; i<(int) fileSize; i++){
                 //System.out.println(allBytes[i]);    
@@ -122,49 +28,58 @@ public class DigestCalculator {
                 buf.append((hex.length() < 2 ? "0" : "") + hex);
             }     
             
+            String digest_string = buf.toString();
+
             // imprime o digest em hexadecimal
             System.out.println( "\nDigest(hex): " );
-            System.out.println( buf.toString() );
-            
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
+            System.out.println(digest);
+            return digest_string;
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
         
     }
     public static void main (String[] args) throws Exception {
-        
-        //Cria arquivo xml no formato especificado
-        CreateXMLFileInJava();
-        
+
         //check args
-        if (args.length != 4) {
+        if (args.length != 3) {
           System.err.println("Usage: DigestCalculator<SP>Tipo_Digest<SP>Caminho_da_Pasta_dos_Arquivos<SP>Caminho_ArqListaDigest");
           System.exit(1);
         }
         
-        if (!args[1].equals("MD5") &&
-                !args[1].equals("SHA1") &&
-                !args[1].equals("SHA256") &&
-                !args[1].equals("SHA512")) {
-            System.err.println("tipo de digest invalido");
+        if (!args[0].equals("MD5") &&
+                !args[0].equals("SHA1") &&
+                !args[0].equals("SHA256") &&
+                !args[0].equals("SHA512")) {
+            System.err.println("Tipo de digest inválido\nTipos suportados: MD5, SHA1, SHA256, SHA512");
             System.exit(2);
         }
         
-        String tipo_digest = args[1];
-        String dir_path = args[2];
+        String digest_type = args[0];
+        String directory_path = args[1];
+        String xml_path = args[2];
+
+        XmlHandler xml_handler = new XmlHandler(xml_path);
         
-        File fObj = new File(dir_path);  
+        File fObj = new File(directory_path);
         if(fObj.exists() && fObj.isDirectory()){
             
             File a[] = fObj.listFiles();
             
             for(File arquivo : a){
                 System.out.println("\n" + arquivo.getPath());
-                System.out.println(arquivo.getName());
-                String inputFile = arquivo.getPath();
-                file_digest(inputFile, tipo_digest);
                 
+                String inputFile = arquivo.getPath();
+
+                String digest = file_digest(inputFile, digest_type);
+                XmlHandler.Status file_status = xml_handler.queryFileDigest(arquivo.getName(), digest_type, digest);
+                
+                System.out.println(String.format("%s %s %s (%s)",arquivo.getName(),digest_type,digest,file_status));
             }
+
+            xml_handler.SaveFile();
         }
         else{
             System.err.println("O caminho indicado não é um diretório.");
---
+++ XmlHandler.java
@@ -0,0 +1,175 @@
+import java.io.*;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+public class XmlHandler {
+    private String catalog_path;
+    private Document xml_catalog;
+
+    public enum Status {
+        OK, NOT_OK, NOT_FOUND, COLLISION, ERROR
+    }
+
+    // XML Handler constructor, either creates new XML catalog or reads existing one
+    public XmlHandler(String xml_path) {
+        catalog_path = xml_path;
+        File f = new File(catalog_path);
+        try {
+            if (f.exists() && !f.isDirectory()) { 
+                // XML catalog does not exist => create it
+                Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+                
+                // add root to Document
+                doc.appendChild(doc.createElement("CATALOG"));
+
+                xml_catalog = doc;
+            } else {
+                // read from existing XML catalog
+                xml_catalog = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(f);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Exports XML catalog to file at given path
+    public void SaveFile() {
+        try {
+            // for output to file, console
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+
+            // for pretty print
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            DOMSource source = new DOMSource(xml_catalog);
+
+            // write to console or file
+            StreamResult console = new StreamResult(System.out);
+            StreamResult file = new StreamResult(new File(catalog_path));
+
+            // write data
+            transformer.transform(source, console);
+            transformer.transform(source, file);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // returns a file entry from the catalog
+    // used when adding new digest codes when they're not found in the catalog
+    private Element GetFileEntry(String file_name) throws Exception {
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        String expression = String.format("/CATALOG/FILE_ENTRY[child::FILE_NAME[text()='%s']]",file_name);
+
+        // query for existing entry
+        NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(xml_catalog, XPathConstants.NODESET);
+        if (nodeList.getLength()>0) return (Element) nodeList.item(0); // return existing entry
+        return null;
+    }
+
+    // creates new digest entry and returns it
+    private Element createDigestEntry(String digest_type, String file_digest) {
+        // create digest collection
+        Element digest_entry_node = xml_catalog.createElement("DIGEST_ENTRY");
+
+        // create digest_type
+        Element digest_type_node = xml_catalog.createElement("DIGEST_TYPE");
+        digest_type_node.appendChild(xml_catalog.createTextNode(digest_type));
+
+        // create digest_hex
+        Element digest_hex_node = xml_catalog.createElement("DIGEST_HEX");
+        digest_hex_node.appendChild(xml_catalog.createTextNode(file_digest));
+
+        // append those to digest entry
+        digest_entry_node.appendChild(digest_type_node);
+        digest_entry_node.appendChild(digest_hex_node);
+
+        return digest_entry_node;
+    }
+
+    // checks if there is digest collision
+    // returns True if there is, False if there isn't
+    private boolean checkDigestCollision(String file_name, String digest_type, String file_digest) throws Exception {
+        // query entries by digest
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        String expression = String.format("/CATALOG/FILE_ENTRY/DIGEST_ENTRY[child::DIGEST_HEX[text()='%s']]",file_digest);
+        NodeList fileEntriesQueryResult = (NodeList) xPath.compile(expression).evaluate(xml_catalog, XPathConstants.NODESET);
+
+        // obvious cases
+        if (fileEntriesQueryResult.getLength() == 0) return false;
+        if (fileEntriesQueryResult.getLength() > 1) return true;
+
+        // checking if the one result is the file itself or some other
+        Element file_entry = (Element) fileEntriesQueryResult.item(0);
+        String catalog_file_name = file_entry.getElementsByTagName("FILE_NAME").item(0).getNodeValue();
+
+        // if the catalog file name is different, there is a collision
+        return !catalog_file_name.equals(file_name);
+    }
+
+    public Status queryFileDigest(String file_name, String digest_type, String file_digest) {
+        try {
+            // collision check
+            if (checkDigestCollision(file_name, digest_type, file_digest)) return Status.COLLISION;
+
+            Element file_entry = GetFileEntry(file_name);
+            if (file_entry != null) {
+                XPath xPath = XPathFactory.newInstance().newXPath();
+                String expression = String.format("/DIGEST_ENTRY[child::DIGEST_TYPE[text()='%s']]",digest_type);
+                NodeList digestQueryResult = (NodeList) xPath.compile(expression).evaluate(file_entry, XPathConstants.NODESET);
+
+                if (digestQueryResult.getLength() == 0) {
+                    // new digest type for the given file
+                    // create digest entry
+                    Element digest_entry_node = createDigestEntry(digest_type, file_digest);
+                    file_entry.appendChild(digest_entry_node);
+
+                    return Status.NOT_FOUND; 
+                }
+
+                Element digestEntry = (Element) digestQueryResult.item(0);
+                String catalogFileDigest = digestEntry.getElementsByTagName("DIGEST_HEX").item(0).getNodeValue();
+
+                if (catalogFileDigest.equals(file_digest)) return Status.OK;
+                else return Status.NOT_OK;
+
+            } else {
+                // no original file entry found
+                // create file entry
+                Element root = xml_catalog.getDocumentElement();
+                file_entry = xml_catalog.createElement("FILE_ENTRY");
+
+                // create FILE_NAME
+                Element file_name_node = xml_catalog.createElement("FILE_NAME");
+                file_name_node.appendChild(xml_catalog.createTextNode(file_name));
+                file_entry.appendChild(file_name_node);
+
+                // create digest entry
+                Element digest_entry_node = createDigestEntry(digest_type, file_digest);
+                file_entry.appendChild(digest_entry_node);
+                
+                // append file entry to catalog
+                root.appendChild(file_entry);
+                
+                return Status.NOT_FOUND; 
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return Status.ERROR;
+        }
+    }
+}
